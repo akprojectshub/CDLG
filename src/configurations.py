02529924c3d3c03ab5024dece8ed30b75d42cd31
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2024-02-01 18:27:21 +0100

    Update the config file and the new input scenario scdd

--- src/configurations.py
+++ src/configurations.py
@@ -3,7 +3,7 @@ import os
 # Default directories
 DEFAULT_OUTPUT_DIR = os.path.join('output')
 DEFAULT_PARAMETER_DIR = os.path.join('src/input_parameters/')
-PARAMETER_NAME = 'scdd'
+PARAMETER_NAME = 'default'
 # First time stamp of each event log
 FIRST_TIMESTAMP = '2024/01/01 08:00:00'
 # Incremental evolution parameters for incremental drifts
